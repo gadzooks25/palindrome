ebdbd8a8d2ac8bd54086a4cb0ea14472700363ba
Justin Dean <deanplums@gmail.com>
2017-11-17 10:02:09 -0700

    initial palindrome

---
+++ palindrome.rb
@@ -0,0 +1,33 @@
+require 'pry'
+require 'colorize'
+
+puts 'Palindrome Checker'.red
+puts 'Enter below to check if you have a palindrome'.magenta
+
+#can use procs but can't remember how, what about load array?
+#def initialize(pal)
+#  @pal = pal
+#end
+@pal = []
+binding.pry
+#class
+#  def input.@pal
+#  end
+#end
+input = gets >> [@pal]
+binding.pry
+#input >> [@pal]
+
+
+
+
+
+#case user_input
+#  when 1
+#    contacts.each do |input|
+#    puts "Palindrome: #{input}"
+#    exit
+#  else
+#    puts 'Bad input'
+#  end
+#end*/
